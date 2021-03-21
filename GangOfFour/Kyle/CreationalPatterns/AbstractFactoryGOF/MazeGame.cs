aadc6c43ba4cf16fd6376cfa0604b5bbe63389dc
Kyle Givler <xvegan88x@gmail.com>
2021-03-21 12:16:06 -0400

    Start implementing Abstract Factory Pattern

---
+++ GangOfFour/Kyle/CreationalPatterns/AbstractFactoryGOF/MazeGame.cs
@@ -0,0 +1,36 @@
+﻿using GOFLibrary.Maze;
+using GOFLibrary.Maze.MapSite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryGOF
+{
+    public class MazeGame
+    {
+        Maze CreateMaze (MazeFactory factory)
+        {
+            Maze maze = factory.MakeMaze();
+            Room r1 = factory.MakeRoom(1);
+            Room r2 = factory.MakeRoom(2);
+            Door door = factory.MakeDoor(r1, r2);
+
+            maze.AddRoom(r1);
+            maze.AddRoom(r2);
+
+            r1.SetSide(Direction.North, factory.MakeWall());
+            r1.SetSide(Direction.East, door);
+            r1.SetSide(Direction.South, factory.MakeWall());
+            r1.SetSide(Direction.West, factory.MakeWall());
+
+            r2.SetSide(Direction.North, factory.MakeWall());
+            r2.SetSide(Direction.East, factory.MakeWall());
+            r2.SetSide(Direction.South, factory.MakeWall());
+            r2.SetSide(Direction.West, door);
+
+            return maze;
+        }
+    }
+}
--- GangOfFour/Kyle/CreationalPatterns/AbstractFactoryGOF/Program.cs
+++ GangOfFour/Kyle/CreationalPatterns/AbstractFactoryGOF/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * Abstract Factory Demo from the GOF Design Patter Book (87)
+ * 
+ * Intent: Provide an interface for creating families of related 
+ * or dependant objects without specifing their concrete classes
+ * 
+ * Also know as: Kit
+ * 
+ * Participants:
+ * AnstractFactory: Interface for operations that create abstract product objects
+ * ConcreteFactory: Implement the operations to create concrete product objects
+ * AbstractProduct: Declares an interface for a type of product object
+ * ConcreteProduct: defines a product object to be created by the corresponding concrete factory, implements AbstractProduct interface
+ * Client: Uses only interfaces declared by AbstractFactory and AbstractProduct classes
+ * 
+ * See Also:
+ * DI vs Factory Pattern: https://stackoverflow.com/questions/557742/dependency-injection-vs-factory-pattern
+ */
+
+using System;
 
 namespace AbstractFactory
 {
