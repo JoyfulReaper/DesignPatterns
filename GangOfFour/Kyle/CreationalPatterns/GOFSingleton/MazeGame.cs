27b4a12064d7c312509f955ea42eae68f7a333a9
Kyle Givler <xvegan88x@gmail.com>
2021-04-01 21:42:18 -0400

    Singleton example

---
+++ GangOfFour/Kyle/CreationalPatterns/GOFSingleton/MazeGame.cs
@@ -0,0 +1,33 @@
+﻿using GOFLibrary.Maze;
+using GOFLibrary.Maze.MapSite;
+
+// Example of creating a Maze using the Abstract Factory Pattern
+
+namespace GOFSingleton
+{
+    public class MazeGame
+    {
+        public Maze CreateMaze (MazeFactory factory)
+       {
+            Maze maze = factory.MakeMaze();
+            Room r1 = factory.MakeRoom(1);
+            Room r2 = factory.MakeRoom(2);
+            Door door = factory.MakeDoor(r1, r2);
+
+            maze.AddRoom(r1);
+            maze.AddRoom(r2);
+
+            r1.SetSide(Direction.North, factory.MakeWall());
+            r1.SetSide(Direction.East, door);
+            r1.SetSide(Direction.South, factory.MakeWall());
+            r1.SetSide(Direction.West, factory.MakeWall());
+
+            r2.SetSide(Direction.North, factory.MakeWall());
+            r2.SetSide(Direction.East, factory.MakeWall());
+            r2.SetSide(Direction.South, factory.MakeWall());
+            r2.SetSide(Direction.West, door);
+
+            return maze;
+        }
+    }
+}
---
+++ GangOfFour/Kyle/CreationalPatterns/GOFSingleton/Program.cs
@@ -0,0 +1,43 @@
+﻿/**
+Singleton Demo from the GOF Design Patter Book (117)
+
+Intent: 
+Ensure a class only has one instance and provide a global point of access to it.
+
+Also known as:
+
+Applicability: 
+There must be only one instance of a class, and it must be accessible to clients
+from a well-known acess point
+
+Collaborations:
+Clients access a Singleton instance solely through Singleton's Instance operation
+
+Participants:
+Singleton - defines an Instance operation that lets clients access its unique instance.
+Instance is a class operation. May be responisble for creating its own unique instance.
+
+See Also:
+*/
+
+using System;
+
+namespace GOFSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor org = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Design Pattern Demos: Traditional Gang of Four Examples: Creational Design Patterns: Singleton");
+            Console.WriteLine("https://github.com/JoyfulReaper\n");
+            Console.ForegroundColor = org;
+
+            Console.WriteLine("Creating Maze using MazeFactory singleton");
+            MazeGame mazeGame = new MazeGame();
+            var maze = mazeGame.CreateMaze(MazeFactory.Instance());
+        }
+    }
+}
