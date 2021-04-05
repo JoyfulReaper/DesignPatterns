269d2b49e7590667b53f26a4a938420d060cc82a
Kyle Givler <xvegan88x@gmail.com>
2021-04-04 21:01:34 -0400

    Abstract Factory Demo

---
+++ GangOfFour/Kyle/DesignPatternExamples/AbstractFactory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimalFactory factory = new DogFactory();
+
+            IAnimal a1 = factory.MakeAnimal();
+
+            factory = new CatFactory();
+
+            IAnimal a2 = factory.MakeAnimal();
+
+            Console.WriteLine($"a1 says: {a1.Speak()}");
+            Console.WriteLine($"a2 says: {a2.Speak()}");
+        }
+    }
+}
