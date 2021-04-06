6bb8f4926cd26f4cb8a1919f82a89249a3dab2a1
Kyle Givler <xvegan88x@gmail.com>
2021-04-06 18:57:28 -0400

    Singleton Example

---
+++ GangOfFour/Kyle/DesignPatternExamples/Singleton/RandomAnimalName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class RandomAnimalName : IRandomWord
+    {
+        private static readonly RandomAnimalName _instance = new RandomAnimalName();
+
+        private List<string> _words = new List<string> { "Cat", "Dog", "Deer", "Panda", "Bat" };
+        Random rand = new Random();
+
+        private RandomAnimalName()
+        {
+
+        }
+
+        public static IRandomWord GetInstance()
+        {
+            return _instance;
+        }
+
+        public string getRandomWord()
+        {
+            return _words[rand.Next(_words.Count())];
+        }
+    }
+}
---
+++ GangOfFour/Kyle/DesignPatternExamples/Singleton/RandomName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class RandomName : IRandomWord
+    {
+        private static readonly RandomName _instance = new RandomName();
+
+        private List<string> _words = new List<string> { "Sam", "Sally", "Fred", "Ethel", "Tim" };
+        Random rand = new Random();
+
+        private RandomName()
+        {
+
+        }
+
+        public static IRandomWord GetInstance()
+        {
+            return _instance;
+        }
+
+        public string getRandomWord()
+        {
+            return _words[rand.Next(_words.Count())];
+        }
+    }
+}
