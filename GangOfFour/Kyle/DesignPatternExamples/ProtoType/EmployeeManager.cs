2e2aa38544a7f554a69f8678a60c3f5e7e225162
Kyle Givler <xvegan88x@gmail.com>
2021-04-06 17:05:04 -0400

    Add Prototype Demo

---
+++ GangOfFour/Kyle/DesignPatternExamples/ProtoType/EmployeeManager.cs
@@ -0,0 +1,55 @@
+﻿/*
+MIT License
+Copyright(c) 2021 Kyle Givler
+https://github.com/JoyfulReaper
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ProtoType
+{
+    public class EmployeeManager : IEnumerable<IEmployee>
+    {
+        private readonly Dictionary<string, IEmployee> _prototypes = new Dictionary<string, IEmployee>();
+
+        public IEmployee this[string key]
+        {
+            get
+            {
+                return _prototypes[key] ?? null;
+            }
+            set
+            {
+                _prototypes.Add(key, value);
+            }
+        }
+
+        public IEnumerator<IEmployee> GetEnumerator()
+        {
+            return _prototypes.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ GangOfFour/Kyle/DesignPatternExamples/ProtoType/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+MIT License
+Copyright(c) 2021 Kyle Givler
+https://github.com/JoyfulReaper
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
+Prototype Demo from the GOF Design Patter Book (117)
+
+Intent: 
+Specify the kinds of objects to create using a prototypical instance, and
+create new object by copying this prototype.
+
+Also known as:
+
+Applicability: 
+System should be independent of how its products are created, composed, and represented and:
+Classes to instantiate are specified at run-time/dynamic loading
+Avoid building a class hierarchy of factories that parallels the class hierarchy of products
+Instances of a class can have one of only a few different combinations of state.
+
+Collaborations:
+Client asks a prototype to clone itself
+
+Participants:
+Prototype (IEmployee): Declares an interface for cloning itself
+ConcretePrototype (Employee):  implements an operation for cloning itself
+Client (Program): Create a new object by asking a prototype to clone itself.
+
+See Also:
+*/
+
+using System;
+
+namespace ProtoType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee empKyle = new Employee()
+            {
+                FirstName = "Kyle",
+                LastName = "Givler",
+                Id = 1,
+                PhoneNumber = "555-555-5556",
+                Address = new Address()
+                {
+                    AddressLineOne = "123 Main St",
+                    City = "New York",
+                    State = "PA",
+                    ZipCode = "12345"
+                }
+            };
+
+            EmployeePrototypeFactory factory = new EmployeePrototypeFactory(empKyle);
+            EmployeeManager employeeManager = new EmployeeManager();
+
+            employeeManager[empKyle.FullName] = empKyle;
+
+
+
+            IEmployee empDerek = factory.MakeEmployee();
+            empDerek.Initiliaze("Derek",  "Givler", 2);
+            employeeManager[empDerek.FullName] = empDerek;
+
+            IEmployee empChris = factory.MakeEmployee();
+            empChris.Initiliaze("Chris", "Givler", 3,
+                new Address
+                {
+                    AddressLineOne = "75 Back St",
+                    City = "Tyler",
+                    State = "TX",
+                    ZipCode = "54321"
+                });
+            employeeManager[empChris.FullName] = empChris;
+
+            foreach(IEmployee emp in employeeManager)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
