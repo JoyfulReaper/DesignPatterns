8bb0fb8a4cecd10d3960dd64736e28d6f5b50012
Kyle Givler <xvegan88x@gmail.com>
2021-04-06 12:13:49 -0400

    Add Factory Method

---
+++ GangOfFour/Kyle/DesignPatternExamples/FactoryMethod/PeanutButterAndGrapeJelly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class PeanutButterAndGrapeJelly : ISandwhich
+    {
+        public List<IIngredient> Ingredients { get; } = new List<IIngredient>();
+
+        public PeanutButterAndGrapeJelly()
+        {
+            CreateIngredients();
+        }
+
+        public void CreateIngredients()
+        {
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new PeanutButter());
+            Ingredients.Add(new GrapeJelly());
+            Ingredients.Add(new Bread());
+        }
+    }
+}
